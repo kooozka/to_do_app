f2b2104a5edb6d06a7c2bb7d9a44ddafcb0ab910
kooozka <jkozanecki@op.pl>
2023-06-06 19:54:07 +0200

    Task and data managers added

---
+++ data_manager.py
@@ -0,0 +1,120 @@
+import sqlite3, datetime, task_manager
+from task_manager import Task
+
+class DatabaseConfig:
+        DATABASE_NAME = "todoapp_db"
+
+def create_database():
+        conn = sqlite3.connect(DatabaseConfig.DATABASE_NAME)
+
+        c = conn.cursor()
+        c.execute('''
+                CREATE TABLE Tasks (
+                task_id INTEGER PRIMARY KEY,
+                title TEXT,
+                category INTEGER,
+                description TEXT,
+                priority INTEGER,
+                deadline DATE,
+                completed INTEGER,
+                user_login TEXT,
+                FOREIGN KEY (user_login) REFERENCES Users(login))''')
+        
+        c.execute('''
+                CREATE TABLE Users (
+                login TEXT PRIMARY KEY,
+                password TEXT
+                )
+        ''')
+        conn.commit()
+        conn.close()
+
+def add_task(title: str, category, description: str, priority, deadline: datetime, login: str):
+    conn = sqlite3.connect(DatabaseConfig.DATABASE_NAME)
+    c = conn.cursor() 
+    c.execute("INSERT INTO Tasks (title, category, description, priority, deadline, user_login) VALUES (?, ?, ?, ?, ?, ?)",
+              (title, category, description, priority, deadline, login))
+    conn.commit()
+    conn.close()
+
+def add_user(login: str, password: str):
+    conn = sqlite3.connect(DatabaseConfig.DATABASE_NAME)
+    c = conn.cursor() 
+    #c.execute("SELCT * FROM Users WHERE login = ?", (login,))
+    #user = c.fetchall()
+    #if not user:
+    c.execute("INSERT INTO Users (login, password) VALUES (?, ?)",
+              (login, password))
+    conn.commit()
+    conn.close()
+
+def login_success(login: str, password: str):
+    conn = sqlite3.connect(DatabaseConfig.DATABASE_NAME)
+    c = conn.cursor() 
+    c.execute("SELECT * FROM Users WHERE login = ? AND password = ?", (login, password))
+    result = c.fetchone()
+    conn.commit()
+    conn.close()
+    if result:
+         return True
+    return False
+'''
+
+def add_task(task: Task):
+    conn = sqlite3.connect(DatabaseConfig.DATABASE_NAME)
+    c = conn.cursor() 
+    c.execute("INSERT INTO Tasks (title, category, description, priority, deadline, completed) VALUES (?, ?, ?, ?, ?, ?)",
+              (task.title, task.category, task.description, task.priority, task.deadline, task.completed))
+    conn.commit()
+    conn.close()
+ 
+def get_tasks():
+    conn = sqlite3.connect(DatabaseConfig.DATABASE_NAME)
+    c = conn.cursor()
+    c.execute("SELECT * FROM Tasks")
+    tasks = c.fetchall()
+    conn.commit()
+    conn.close()   
+    return tasks
+'''
+def get_tasks():
+    conn = sqlite3.connect(DatabaseConfig.DATABASE_NAME)
+    c = conn.cursor()
+    c.execute("SELECT * FROM Tasks")
+    db_tasks = c.fetchall()
+    conn.commit()
+    conn.close()
+    tasks = []
+    for db_task in db_tasks:
+          task = Task(db_task[0], db_task[1], task_manager.Categories(db_task[2]), db_task[3], task_manager.Priority(db_task[4]), db_task[5], db_task[6])
+          tasks.append(task)
+    return tasks
+
+def get_user_tasks(login):
+    conn = sqlite3.connect(DatabaseConfig.DATABASE_NAME)
+    c = conn.cursor()
+    c.execute("SELECT * FROM Tasks WHERE user_login = ?", (login,))
+    db_tasks = c.fetchall()
+    conn.commit()
+    conn.close()
+    tasks = []
+    for db_task in db_tasks:
+          task = Task(db_task[0], db_task[1], task_manager.Categories(db_task[2]), db_task[3], task_manager.Priority(db_task[4]), db_task[5], db_task[6])
+          tasks.append(task)
+    return tasks
+
+
+def remove_task(task_id):
+    conn = sqlite3.connect(DatabaseConfig.DATABASE_NAME)
+    c = conn.cursor()
+    c.execute("DELETE FROM Tasks WHERE task_id = ?", (task_id,))  
+    conn.commit()
+    conn.close()
+
+def update_task(id, title: str, category, description: str, priority, deadline: datetime, login: str):
+    conn = sqlite3.connect(DatabaseConfig.DATABASE_NAME)
+    c = conn.cursor()
+    c.execute("UPDATE Tasks SET title = ?, category = ?, description = ?, priority = ?, deadline = ?, user_login = ? WHERE task_id = ?",
+              (title, category, description, priority, deadline, login, id))
+    conn.commit()
+    conn.close()
---
+++ task_manager.py
@@ -0,0 +1,61 @@
+import datetime
+from enum import Enum
+
+class Priority(Enum):
+    HIGH = 1
+    MEDIUM = 2
+    LOW = 3
+
+class Categories(Enum):
+    FINANCES = 1
+    FAMILY = 2
+    MEETINGS = 3
+    WORK = 4
+    HOME = 5
+
+class Task:
+    def __init__(self, task_id: int, title: str, category: Categories, description: str, priority: Priority, deadline: datetime, user_login: str):
+        self.task_id = task_id
+        self.title = title
+        self.category = category
+        self.description = description
+        self.priority = priority
+        self.deadline = deadline
+        self.user_login = user_login
+
+    def mark_as_completed(self):
+        self.completed = True
+
+    def mark_as_incompleted(self):
+        self.completed = False
+
+    def __str__(self) -> str:
+        return f"{self.task_id}    {self.title}\t   \t    {self.priority.name}       \t  {self.deadline}"
+    
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task: Task):
+        self.tasks.append(task)
+
+    def add_tasks(self, new_tasks):
+        self.tasks.extend(new_tasks)
+
+    def remove_task(self, task: Task):
+        self.tasks.remove(task)
+
+    def get_all_tasks(self):
+        return self.tasks
+    
+    def get_task_by_id(self, task_id):
+        for task in self.tasks:
+            if task.task_id == task_id:
+                return task
+        return None
+    
+    def get_tasks_sorted_by_priority(self):
+        return sorted(self.tasks, key=lambda task: (task.priority.value, task.deadline), reverse = False)
+    
+    def get_tasks_sorted_by_deadline(self):
+        return sorted(self.tasks, key=lambda task: task.deadline, reverse = False)
